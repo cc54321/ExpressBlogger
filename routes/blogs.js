cbc8d1e84d1a06e6d83d502c0487e95c7a80bf4c
cc54321 <charles.cole@codeimmersives.com>
2023-02-03 22:20:57 -0500

    commit -a

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,5 +1,8 @@
+const { application } = require('express');
 const express = require('express');
 const router = express.Router();
+
+const { validateBlogs } = require("../validation/blogs");
 
 const sampleBlogs = [
   {
@@ -59,7 +62,7 @@ router.get("/all", (req, res) => {
 });
 
 router.get("/single/:title",function (req, res){
-  const singleblog =sampleBlogs.find((blog)=>{
+  const singleblog = sampleBlogs.find((blog)=>{
     return blog.title === req.params.title
 
   })
@@ -68,6 +71,116 @@ router.get("/single/:title",function (req, res){
     blog: singleblog
   })
 })
+
+router.post("/blogs/create-one", (req, res)=>{
+    if (req.body.blogTitle === undefined || typeof(req.body.blogTitle) !== "string"){
+      res.json({
+        success: false,
+        message: "blog title is reqiured and must be a string"
+      })
+      return
+    }
+    if (req.body.blogText === undefined || typeof(req.body.blogText) !== "string"){
+      res.json({
+        success: false,
+        message: "blog text is required and must be a string"
+      })
+      return 
+    }
+    if (req.body.blogAuthor === undefined || typeof(req.body.blogAuthor) !== "string"){
+       res.json({
+        success: false,
+        message: "author is required and must be a string"
+       })
+       return 
+    } 
+      
+     
+     const blogTitle = req.body.blogTitle
+     const blogText = req.body.blogText
+     const blogAuthor = req.body.blogAuthor
+
+     const sampleBlogData = {
+      blogTitle: blogTitle,
+      blogText: blogText,
+      blogAuthor: blogAuthor,
+      createdAt: new Date(),
+      lastModified: new Date(),
+
+     }
+
+     sampleBlogs.push(sampleBlogData)
+
+     res.json({
+       success: true,
+       
+  
+
+  })
+   
+  
+  
+
+      
+    })
+
+router.put('/update-one/:blogTitle', (req, res)=>{
+    const sampleBlogToFind = req.params.blogTitle
+
+    const originalBlog = blogs.find((blog)=>{
+
+      return blog.title === sampleBlogToFind
+
+     })
+
+     const sampleBlogIndex = blogs.findIndex((blog)=>{
+
+      return sampleBlogIndex === sampleBlogToFind
+     })
+
+     if (!originalBlog) {
+      res.json({
+        success: false,
+        message: "blog not found"
+      })
+      return
+    }
+    const updatedBlog = {}
+    
+      if (req.body.title !== undefined){
+        updatedBlog.title = req.body.title
+      } else {
+        updatedBlog.title = originalBlog.title
+      }
+    
+      if (req.body.text !== undefined){
+        updatedBlog.text = req.body.text
+      } else {
+        updatedBlog.text = originalBlog.text
+      }
+    
+      if (req.body.author !== undefined){
+        updatedBlog.author = req.body.author
+      } else {
+        updatedBlog.author = originalBlog.author
+      }
+
+      updatedBlog.createdAt = new Date
+      updatedBlog.lastModified = new Date
+
+      blogs[blogIndex] = updatedBlog
+
+      console.log(blogs)
+      res.json({
+        success: true,
+        updatedBlog: updatedBlog,
+        blogs: blogs
+        
+      })
+    })
+
+
+  
 
 
 router.delete("/delete/:title",function(req,res){
--- validation/blogs.js
+++ validation/blogs.js
@@ -1,1 +1,28 @@
 
+if (blogData.title === undefined || typeof(blogData.title) !== "string" || blogData.title.length > 40) {
+    //blog data must be of type string
+    return {
+        isValid: false,
+        message: "title is required and must be of type string less than 40 characters"
+    }
+}
+
+if (blogData.text === undefined || typeof(blogData.text) !== "string") {
+    
+    return {
+        isValid: false,
+        message: "blog is required and must be of type string"
+    }
+}
+
+if (blogData.author === undefined || typeof(blogData.author) !== "string" || blogData.author.length > 40 ) {
+    // lastName is required and it must be a string
+    return {
+        isValid: false,
+        message: "Author is required and it must be a string and less than 40 characters"
+    }
+}
+module.exports = {
+	
+	validateBlogs, 
+}
