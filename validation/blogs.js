98a827c20ec05dc44365de788e29c7032275710d
cc54321 <charles.cole@codeimmersives.com>
2023-02-03 23:06:43 -0500

    commit -a

--- validation/blogs.js
+++ validation/blogs.js
@@ -1,28 +1,20 @@
 
-if (blogData.title === undefined || typeof(blogData.title) !== "string" || blogData.title.length > 40) {
-    //blog data must be of type string
-    return {
-        isValid: false,
-        message: "title is required and must be of type string less than 40 characters"
-    }
-}
-
-if (blogData.text === undefined || typeof(blogData.text) !== "string") {
+// if (sampleBlogData.text === undefined || typeof(sampleBlogData.text) !== "string") {
     
-    return {
-        isValid: false,
-        message: "blog is required and must be of type string"
-    }
-}
+//     return {
+//         isValid: false,
+//         message: "blog is required and must be of type string"
+//     }
+// }
 
-if (blogData.author === undefined || typeof(blogData.author) !== "string" || blogData.author.length > 40 ) {
-    // lastName is required and it must be a string
-    return {
-        isValid: false,
-        message: "Author is required and it must be a string and less than 40 characters"
-    }
-}
-module.exports = {
+// if (sampleBlogData.author === undefined || typeof(sampleBlogData.author) !== "string") {
+//     // lastName is required and it must be a string
+//     return {
+//         isValid: false,
+//         message: "Author required and  must be a string"
+//     }
+// }
+// module.exports = {
 	
-	validateBlogs, 
-}
+// 	validateBlogs, 
+// }
